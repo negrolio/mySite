0d2aa37d6fd2f7e77355c439ae9e61b6ba6db13f
negrolio <elnegroleo9@gmail.com>
2017-11-22 13:50:12 -0300

    proof to send email with formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                //url: "././mail/contact_me.php",
+                url: "https://formspree.io/leonardoalbertomolinari@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
